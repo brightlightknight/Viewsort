41602b4704266dbe91cdf01ca4b8ebbcc0bfc32e
TheMegax <mathias.tello12@gmail.com>
2024-10-10 15:07:40 -0300

    Reformatting

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import asyncio
 import datetime
+import logging
 import os
 import sqlite3
 from datetime import timedelta
 from TikTokApi import TikTokApi
-
-max_fetch = 1
+from TikTokApi.api.video import Video
 
 ms_token = os.environ.get("ms_token", None)
 
-
-async def crawl_videos():
-    global max_fetch
-
-    async with TikTokApi() as api:
-        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, browser="webkit")
-        async for video in api.trending.videos(count=max_fetch):
-            async for related_video in video.related_videos(count=max_fetch):
-                video_id = int(related_video.id)
-                likes = int(related_video.stats['diggCount'])
-
-                today = datetime.datetime.now()
-                date = related_video.create_time
-                if today - date < timedelta(days=30):
-                    await insert_tiktok_video(video_id, likes, date)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def create_tables():
+async def crawl_videos(videos: list[Video], video_fetch=30, depth=0, max_depth=4):
+    logging.info(f"-- Depth: {depth}.")
+    for video in videos:
+        for related_video in video.related_videos(count=video_fetch):
+            video_id = int(related_video.id)
+            likes = int(related_video.stats['diggCount'])
+
+            today = datetime.datetime.now()
+            date = related_video.create_time
+
+            # Here we check if the video isn't too old. If it is, ignore it for the crawl.
+            if today - date < timedelta(days=30):
+                await insert_tiktok_video(video_id, likes, date)
+                if depth + 1 < max_depth:
+                    related_videos = list(related_video.related_videos(count=video_fetch))
+                    await crawl_videos(videos=related_videos, video_fetch=video_fetch, depth=depth + 1)
+
+
+async def create_tables():
     sql_statements = [
         """CREATE TABLE IF NOT EXISTS tiktok_videos (
                 id INTEGER PRIMARY KEY, 
@@ -48,15 +55,27 @@ async def insert_tiktok_video(video_id: int, likes: int, create_date: datetime.d
     try:
         with sqlite3.connect("viewsort.db") as conn:
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO tiktok_videos(id, likes, create_date) VALUES(?,?,?)"
-                           "ON CONFLICT(id)"
-                           "DO UPDATE SET ",
+            cursor.execute("""INSERT INTO tiktok_videos(id, likes, create_date) VALUES(?,?,?)
+                           ON CONFLICT(id) DO UPDATE SET
+                           likes=excluded.likes, 
+                           create_date=excluded.create_date;""",
                            (video_id, likes, str(create_date)))
             conn.commit()
     except sqlite3.Error as e:
         print(e)
 
 
+async def main():
+    logging.info("Initializing...")
+    await create_tables()
+    logging.info("Created tables")
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, browser="webkit")
+        logging.info("Started new user session")
+        init_videos = await api.trending.videos(count=30)
+        logging.info("Got initial videos to crawl")
+        await crawl_videos(videos=init_videos, video_fetch=1, max_depth=2)
+
+
 if __name__ == "__main__":
-    create_tables()
-    asyncio.run(crawl_videos())
+    asyncio.run(main())
