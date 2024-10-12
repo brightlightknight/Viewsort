8bb72cde1a8de6823548939574b6dfef1cf705f1
TheMegax <mathias.tello12@gmail.com>
2024-10-11 21:21:05 -0300

    Added function to get top liked videos

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import logging
 import os
 import sqlite3
 from datetime import timedelta
+from sqlite3 import Connection
+
 from TikTokApi import TikTokApi
 from TikTokApi.api.video import Video
 
@@ -36,6 +38,14 @@ async def crawl_videos(videos: list[Video], video_fetch=30, depth=0, max_depth=4
                     await crawl_videos(videos=related_videos, video_fetch=video_fetch, depth=depth + 1)
 
 
+async def get_connection() -> Connection:
+    try:
+        with sqlite3.connect("viewsort.db") as conn:
+            return conn
+    except sqlite3.Error as e:
+        print(e)
+
+
 async def create_tables():
     sql_statements = [
         """CREATE TABLE IF NOT EXISTS tiktok_videos (
@@ -44,28 +54,32 @@ async def create_tables():
                 create_date TEXT
         );"""
     ]
+    conn = await get_connection()
 
-    try:
-        with sqlite3.connect("viewsort.db") as conn:
-            cursor = conn.cursor()
-            for statement in sql_statements:
-                cursor.execute(statement)
-    except sqlite3.Error as e:
-        print(e)
+    cursor = conn.cursor()
+    for statement in sql_statements:
+        cursor.execute(statement)
+
+
+async def get_tiktok_top_liked_videos(limit: int = 50, offset: int = 0):
+    conn = await get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""  SELECT id, likes, create_date FROM tiktok_videos
+                        ORDER BY likes DESC
+                        LIMIT ? OFFSET ?;""",
+                   (limit, offset))
+    return cursor.fetchall()
 
 
 async def insert_tiktok_video(video_id: int, likes: int, create_date: datetime.datetime):
-    try:
-        with sqlite3.connect("viewsort.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("""INSERT INTO tiktok_videos(id, likes, create_date) VALUES(?,?,?)
-                           ON CONFLICT(id) DO UPDATE SET
-                           likes=excluded.likes, 
-                           create_date=excluded.create_date;""",
-                           (video_id, likes, str(create_date)))
-            conn.commit()
-    except sqlite3.Error as e:
-        print(e)
+    conn = await get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO tiktok_videos(id, likes, create_date) VALUES(?,?,?)
+                    ON CONFLICT(id) DO UPDATE SET
+                    likes=excluded.likes, 
+                    create_date=excluded.create_date;""",
+                   (video_id, likes, str(create_date)))
+    conn.commit()
 
 
 async def main():
